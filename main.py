1b7fd0236ca6971208777f0a27d27d9acb751943
viwoo <60322645+q1auer@users.noreply.github.com>
2022-03-19 14:01:36 +0300

    добавление проверки на valueerror

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 def calculator():
-    n1 = int(input('Введите первое число: '))
-    operator = input('Введите оператора: ')
-    n2 = int(input('Введите второе число: '))
+    try:
+        n1 = int(input('Введите первое число: '))
+        operator = input('Введите оператора: ')
+        n2 = int(input('Введите второе число: '))
 
-    if operator == '+':
-        print(n1 + n2)
-    elif operator == '-':
-        print(n1 - n2)
-    elif operator == '*':
-        print(n1 * n2)
-    elif operator == '/':
-        print(n1 / n2)
-    else:
+        if operator == '+':
+            print(n1 + n2)
+        elif operator == '-':
+            print(n1 - n2)
+        elif operator == '*':
+            print(n1 * n2)
+        elif operator == '/':
+            print(n1 / n2)
+        else:
+            print('Вы ввели недопустимое значение!')
+    except ValueError:
         print('Вы ввели недопустимое значение!')
