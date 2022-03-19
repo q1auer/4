a9c987a8bbcd170aae1e2599609bf9cd00af5ac7
viwoo <60322645+q1auer@users.noreply.github.com>
2022-03-19 13:59:15 +0300

    добавление if,elif,else

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ def calculator():
     n1 = int(input('Введите первое число: '))
     operator = input('Введите оператора: ')
     n2 = int(input('Введите второе число: '))
+
+    if operator == '+':
+        print(n1 + n2)
+    elif operator == '-':
+        print(n1 - n2)
+    elif operator == '*':
+        print(n1 * n2)
+    elif operator == '/':
+        print(n1 / n2)
+    else:
+        print('Вы ввели недопустимое значение!')
